f6b626e7424e3dd3ccba324afe49c1f79f39db3d
SAM-MBP <ainiruoxia@outlook.com>
2023-05-03 17:48:41 +0800

    feat:first commit

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+# ！/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @TIme 2023/5/3 17:28
+# @Author: SAM
+# @File: helloworld.py
+# @Email: SAM-Turentu@outlook.com
+# @Desc:
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
