72b3a11eba545b4d90e74e7fadb83df54080a68e
SAM-Turentu <SAM-Turentu@outlook.com>
2023-05-05 16:19:19 +0800

    fix: 添加一些注释

--- helloworld.py
+++ helloworld.py
@@ -7,8 +7,42 @@
 # @Desc:
 
 
+import tensorflow as tf
+
+
 def main():
-    ...
+    mnist = tf.keras.datasets.mnist
+
+    # 每个图转换为 28 * 28 的二维数组，记录每个像素点的(只有黑色) black rgb值
+
+    file_path = 'E:\\SAM_Projects\\Python_Projects\\LearnTF\\keras_mnist_data\\datasets\\mnist.npz'  # 已经下载的数据集
+    # 加载 mnist 数据集
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(file_path)  # 没有数据集时，自动下载
+    x_train, x_test = x_train / 255, x_test / 255   # 每个单元格 除以 255， 0-1范围内
+
+    # 通过堆叠层构建模型
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10)
+    ])
+    predictions = model(x_train[:1]).numpy()  # numpy.ndarray 类型
+    predictions = tf.nn.softmax(predictions).numpy()  # softmax函数将这些 logits 转换为每个类的概率
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)  # 损失函数，为每个样本返回一个标量损失
+    loss_fn(y_train[:1], predictions).numpy()
+
+    # 配置和编译模型
+    model.compile(optimizer='adam',
+                  loss=loss_fn,
+                  metrics=['accuracy'])  # accuracy 模型评估的指标
+    model.fit(x_train, y_train, epochs=5)
+    model.evaluate(x_test, y_test, verbose=2)  # 检查模型性能
+    probability_model = tf.keras.Sequential([
+        model,
+        tf.keras.layers.Softmax()
+    ])
+    probability_model(x_test[:5])
 
 
 if __name__ == '__main__':
---
+++ learn_np/learn_np.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File    :   learn_np.py
+# @Time    :   2023/5/4
+# @Author  :   SAM
+# @Desc    :   学习 numpy
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
