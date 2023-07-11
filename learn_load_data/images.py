e8f7c54280408372114e4e675c5d6a3c020c9b5e
SAM-MBP <ainiruoxia@outlook.com>
2023-07-11 18:08:26 +0800

    feat: images(使用 tf.data 进行更精细的控制)

--- learn_load_data/images.py
+++ learn_load_data/images.py
@@ -7,6 +7,7 @@
 # @Desc: 加载和预处理图像
 
 
+import os
 import pathlib
 
 import PIL.Image
@@ -31,7 +32,7 @@ data_dir = join_path.load_data.images_path
 
 data_dir = pathlib.Path(data_dir)
 
-# image_count = len(list(data_dir.glob('*/*.jpg')))  # 文件夹中共有3670个图像
+image_count = len(list(data_dir.glob('*/*.jpg')))  # 文件夹中共有3670个图像
 
 # 玫瑰图片文件夹下所有文件的路径列表
 # roses = list(data_dir.glob('roses/*'))
@@ -66,6 +67,7 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
 class_names = train_ds.class_names
 print(train_ds.class_names)  # 在数据集中找到的类名   ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
 
+# region 显示训练集的前9个图像
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
     for i in range(9):
@@ -73,7 +75,8 @@ for images, labels in train_ds.take(1):
         plt.imshow(images[i].numpy().astype('uint8'))
         plt.title(class_names[labels[i]])
         plt.axis('off')
-plt.show()  # 显示训练集的前9个图像
+plt.show()
+# endregion
 
 for image_batch, labels_batch in train_ds:
     print(image_batch.shape)  # 打印张量: (32, 180, 180, 3)  # 这是由32个形状为 180 * 180 * 3 的突袭那个组成的批次，3：最后一个维度是指颜色通道RGB
@@ -89,6 +92,7 @@ image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
 print(np.min(first_image), np.max(first_image))  # 查看是否在 [0,1] 之间
 # endregion
+
 
 # region 配置数据集以提高性能（使用缓冲预获取）
 #  在第一个周期期间加载图像后，Dataset.cache() 将图像保留在内存中。如果数据集太大，可以使用此方法创建高性能的磁盘缓存（数据缓存到磁盘上  tf.data  API 查看具体使用方法）
@@ -129,5 +133,101 @@ model.fit(
     validation_data=val_ds,  # 20% 数据集
     epochs=3
 )
+
+# endregion
+
+
+# region 使用 tf.data 进行更精细的控制
+list_ds = tf.data.Dataset.list_files(str(data_dir / '*/*'), shuffle=False)
+list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+
+for f in list_ds.take(5):
+    print(f.numpy())
+
+class_names = np.array(sorted([item.name for item in data_dir.glob('*') if item.name != 'LICENSE.txt']))
+print(class_names)
+
+# 将数据集拆分为训练集和测试集
+val_size = int(image_count * 0.2)
+train_ds = list_ds.skip(val_size)
+val_ds = list_ds.take(val_size)
+
+# 每个数据集的长度
+print(tf.data.experimental.cardinality(train_ds).numpy())
+print(tf.data.experimental.cardinality(val_ds).numpy())
+
+
+# 编写一个将文件路径转换为 (img, label) 对的短函数
+def get_label(file_path):
+    # 将路径转换为路径分量的列表
+    parts = tf.strings.split(file_path, os.path.sep)
+    one_hot = parts[-2] == class_names
+    return tf.argmax(one_hot)
+
+
+def decode_img(img):
+    img = tf.io.decode_jpeg(img, channels=3)
+    return tf.image.resize(img, [img_height, img_width])  # 调整图像大小
+
+
+def process_path(file_path):
+    label = get_label(file_path)
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
+
+
+# 使用 Dataset.map 创建 img, label对 的数据集
+#   设置`num_parallel_calls`，使多个图像并行加载/处理
+train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+for image, label in train_ds.take(1):
+    print('Image shape: ', image.numpy().shape)
+    print('Label: ', label.numpy())
+
+
+# region 训练的基本方法
+
+def configure_for_performance(ds):
+    # 数据充分打乱，分割为batch，永远重复
+    ds = ds.cache()
+    ds = ds.shuffle(buffer_size=1000)
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+    return ds
+
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+
+# endregion
+
+
+# region 呈现数据
+
+image_batch, labels_batch = next(iter(train_ds))
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image_batch[i].numpy().astype('uint8'))
+    label = labels_batch[i]
+    plt.title(class_names[label])
+    plt.axis('off')
+plt.show()
+
+# endregion
+
+
+# region 继续训练模型
+model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=3
+)
+
+# endregion
+
 
 # endregion
